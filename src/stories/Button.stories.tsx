b0086b9899f6ba8ab78f15ef66546d2997c16135
Charlotte LECONTE <charlotte.leconte91@gmail.com>
2020-11-18 15:25:32 +0100

    feat(StoryBook): init theme with material UI.

--- src/stories/Button.stories.tsx
+++ src/stories/Button.stories.tsx
@@ -1,38 +1,51 @@
 import React from 'react';
 // also exported from '@storybook/react' if you can deal with breaking changes in 6.1
-import { Story, Meta } from '@storybook/react/types-6-0';
+import { Meta } from '@storybook/react/types-6-0';
+import Button from '@material-ui/core/Button';
+import { action } from '@storybook/addon-actions';
 
-import { Button, ButtonProps } from './Button';
 
 export default {
-  title: 'Example/Button',
-  component: Button,
-  argTypes: {
-    backgroundColor: { control: 'color' },
-  },
+  title: 'Material Button',
 } as Meta;
 
-const Template: Story<ButtonProps> = (args) => <Button {...args} />;
+export const Default = () => (
+  <Button onClick={action('Default button clicked')} variant="contained">
+    Default
+  </Button>
+);
 
-export const Primary = Template.bind({});
-Primary.args = {
-  primary: true,
-  label: 'Button',
-};
+export const Primary = () => (
+  <Button color="primary" onClick={action('Primary button clicked')} variant="contained">
+    Primary
+  </Button>
+);
 
-export const Secondary = Template.bind({});
-Secondary.args = {
-  label: 'Button',
-};
+export const Secondary = () => (
+  <Button color="secondary" onClick={action('Secondary button clicked')} variant="contained">
+    Secondary
+  </Button>
+);
 
-export const Large = Template.bind({});
-Large.args = {
-  size: 'large',
-  label: 'Button',
-};
+// export const Primary = Template.bind({});
+// Primary.args = {
+//   primary: true,
+//   label: 'Button',
+// };
 
-export const Small = Template.bind({});
-Small.args = {
-  size: 'small',
-  label: 'Button',
-};
+// export const Secondary = Template.bind({});
+// Secondary.args = {
+//   label: 'Button',
+// };
+
+// export const Large = Template.bind({});
+// Large.args = {
+//   size: 'large',
+//   label: 'Button',
+// };
+
+// export const Small = Template.bind({});
+// Small.args = {
+//   size: 'small',
+//   label: 'Button',
+// };
---
+++ src/stories/styled-button.stories.tsx
@@ -0,0 +1,16 @@
+
+import React from 'react';
+import { action } from '@storybook/addon-actions';
+import { Meta } from '@storybook/react/types-6-0';
+import StyledButton from '../styles/styled-button';
+
+export default {
+  title: 'Styled Button',
+  component: StyledButton,
+} as Meta;
+
+export const Default = () => (
+  <StyledButton onClick={action('Styled button clicked')}>
+    Styled Button
+  </StyledButton>
+);
