e50a3020d8b27fcc8d4f543772bf63cb8d9651e5
louiscasciodev <casciolouis@gmail.com>
2020-12-16 18:03:39 +0100

    config[theme]: reactive theme in MUI and StoryBook

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,4 +1,8 @@
+import { addDecorator, configure } from '@storybook/react';
 
-export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
-}
+import StylesDecorator from './styles-decorator';
+
+addDecorator(StylesDecorator);
+
+// automatically import all files ending in *.stories.js
+configure(require.context('../src/stories', true, /\.stories\.js$/), module);
--- .storybook/styles-decorator.js
+++ .storybook/styles-decorator.js
@@ -3,12 +3,10 @@ import React from "react";
 import CssBaseline from "@material-ui/core/CssBaseline";
 import { MuiThemeProvider } from "@material-ui/core/styles";
 import { StylesProvider } from "@material-ui/styles";
-
 import { ThemeProvider as StyledThemeProvider } from "styled-components";
+import LightTheme from '../src/themes/light-theme';
 
-import LightTheme from '../themes/light-theme';
-
-const StylesDecorator = (storyFn: any) => (
+const StylesDecorator = (storyFn) => (
   <StylesProvider injectFirst>
     <CssBaseline />
     <StyledThemeProvider theme={LightTheme}>
