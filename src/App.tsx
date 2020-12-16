6ff571e632f35565ec9d313fce28f2b72b92481b
louiscasciodev <casciolouis@gmail.com>
2020-12-16 18:43:41 +0100

    config[MUI-conponents-theming]: add theme wrapper into App

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,32 @@
 import React from "react"
-// import { createStyles, makeStyles, Theme } from "@material-ui/core/styles"
 import api from "./apollo/api"
-// import Course from "./pages/course"
 import {  Switch, Route } from "react-router-dom";
 import { ApolloProvider } from "@apollo/client"
+
+import CssBaseline from "@material-ui/core/CssBaseline";
+import { MuiThemeProvider } from "@material-ui/core/styles";
+import { ThemeProvider as StyledThemeProvider } from "styled-components";
 
 import SignIn from "./pages/connection/SignIn"
 import SignUp from "./pages/connection/SignUp"
 
-// const useStyles = makeStyles((theme: Theme) =>
-//   createStyles({
-//     root: {
-//       flexGrow: 1,
-//     },
-//     menuButton: {
-//       marginRight: theme.spacing(2),
-//     },
-//     title: {
-//       flexGrow: 1,
-//     },
-//   })
-// )
+import LightTheme from '../src/themes/light-theme';
 
 export default function ButtonAppBar() {
-  // const classes = useStyles()
 
   return (
     <ApolloProvider client={api}>
-      <div className="App-header" style={{ display: "flex", justifyContent: "center", alignItems: "center" }}>
-        <Switch >
-          <Route exact path ="/sign-in" component={SignIn} />
-          <Route  path="/sign-up" component={SignUp} />
-        </Switch>
-      </div>
+    <CssBaseline />
+      <StyledThemeProvider theme={LightTheme}>
+        <MuiThemeProvider theme={LightTheme}>
+          <div className="App-header" style={{ display: "flex", justifyContent: "center", alignItems: "center" }}>
+          <Switch >
+            <Route exact path ="/sign-in" component={SignIn} />
+            <Route  path="/sign-up" component={SignUp} />
+          </Switch>
+          </div>
+        </MuiThemeProvider>
+      </StyledThemeProvider>
     </ApolloProvider>
   )
 }
