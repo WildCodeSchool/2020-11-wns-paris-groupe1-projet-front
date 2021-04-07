238c4b7fdc4bd36cf9d8e8c7535172273cacc6df
charlottel91 <charlotte.leconte91@gmail.com>
2021-04-07 15:38:34 +0200

    Resolve pb on route.

--- src/router/RouteWithSubRoutes.tsx
+++ src/router/RouteWithSubRoutes.tsx
@@ -3,29 +3,33 @@ import { Redirect, Route } from 'react-router-dom';
 import { IRoute } from './config';
 import { LoginContext, isLoggedIn } from '../context/login';
 
-const RouteWithSubRoutes = (route: IRoute) => (
-  <Suspense fallback={route.fallback}>
-    <LoginContext.Provider value={isLoggedIn}>
-      <Route
-        path={route.path}
-        render={(props) => (route.redirect ? (
-          <Redirect to={route.redirect} />
-        ) : route.private ? (
-          isLoggedIn ? (
+const RouteWithSubRoutes = (route: IRoute) => {
+  const { fallback, path } = route;
+
+  return (
+    <Suspense fallback={fallback}>
+      <LoginContext.Provider value={isLoggedIn}>
+        <Route
+          path={path}
+          render={(props) => (route.redirect ? (
+            <Redirect to={route.redirect} />
+          ) : route.private ? (
+            isLoggedIn ? (
+              route.component && (
+              <route.component {...props} routes={route.routes} />
+              )
+            ) : (
+              <Redirect to="/sign-in" />
+            )
+          ) : (
             route.component && (
             <route.component {...props} routes={route.routes} />
             )
-          ) : (
-            <Redirect to="/sign-in" />
-          )
-        ) : (
-          route.component && (
-          <route.component {...props} routes={route.routes} />
-          )
-        ))}
-      />
-    </LoginContext.Provider>
-  </Suspense>
-);
+          ))}
+        />
+      </LoginContext.Provider>
+    </Suspense>
+  );
+};
 
 export default RouteWithSubRoutes;
