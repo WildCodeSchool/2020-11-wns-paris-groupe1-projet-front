59c71cb6096e2c3bbcf3421c7f974d8bd65d6ec4
charlottel91 <charlotte.leconte91@gmail.com>
2021-04-07 13:38:19 +0200

    feat(calendar): modification in calendar.

--- src/pages/planning/index.tsx
+++ src/pages/planning/index.tsx
@@ -4,7 +4,7 @@ import Calendar from '../../components/calendar/Calendar';
 
 export default function Planning(): JSX.Element {
   return (
-    <Container>
+    <Container maxWidth="lg">
       <Calendar />
     </Container>
   );
